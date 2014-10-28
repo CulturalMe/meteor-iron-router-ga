cd09b0b395154cdf62b218f5f6642402423d70ec
Clement Pigeon <clementpigeon@gmail.com>
2014-10-28 17:10:31 +0100

    update package.js

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-    name: "reywood:iron-router-ga",
-    summary: "Google analytics (universal edition) with some Iron Router sugar for tracking page views.",
+    name: "CulturalMe:iron-router-ga",
+    summary: "Google analytics (universal edition) with some Iron Router sugar for tracking page views. Forked to work with keen-settings",
     version: "0.3.2",
-    git: "https://github.com/reywood/meteor-iron-router-ga.git"
+    git: "https://github.com/CulturalMe/meteor-iron-router-ga"
 });
 
 Package.onUse(function(api) {
     api.versionsFrom("METEOR@0.9.0");
-    api.use("iron:router@0.9.1", "client");
+    api.use(["keen-settings", "iron:router@0.9.4"], "client");
 
     api.addFiles([ "lib/ga.js", "lib/router.js" ], "client");
 });
